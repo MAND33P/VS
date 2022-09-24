9817876e64b2ce352da8e8a6a7ac27316b1dbe2f
mandeep <13114425man@gmail.com>
2022-09-24 15:13:57 +0530

    new

---
+++ yt-master/yt-master/src/App.js
@@ -0,0 +1,23 @@
+import {BrowseRouter, Routes,  Route, BrowserRouter} from 'react-router-dom';
+import {Box} from '@mui/material';
+// importing all the component s at once
+import { ChannelDetails, Feed, Navbar, SearchFeed, VideoDetails } from './components';
+
+const App = () =>   (
+    <BrowserRouter>
+        <Box sx={{backgroundColor: '#000'}}>
+            <Navbar  />
+            <Routes>
+                <Route path ="/" exact element={<Feed/>}      />
+                <Route path ="/video/:id" element= {<VideoDetails/>} />
+                <Route path ="/channel/:id" element= {<ChannelDetails/>} />
+                <Route path ="/search/:searchTerm" element= {<SearchFeed/>} />
+
+            </Routes>
+
+        </Box>
+    </BrowserRouter>
+  )
+
+
+export default App
